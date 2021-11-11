607e1ca189cb9954b46916a0a592c45a4dc1d175
M3L-N1K <93345052+M3L-N1K@users.noreply.github.com>
2021-11-11 16:40:31 +0300

    T-01
    T-01

---
+++ src/com/company/main/Main.java
@@ -0,0 +1,18 @@
+package com.company.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    char ch;
+
+        ch = 'L';
+        System.out.println("ch равно " + ch);
+
+        ch++;
+        System.out.println("Значение ch измениось на " + ch);
+
+        ch = 'Д';
+        ch--;
+        System.out.println(" Значение ch снова изменилоь и равно " + ch);
+    }
+}
